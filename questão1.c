96d612609eb368e4ef0f4ea1fc477c325d5526d7
davi522 <davipereira47626@gmail.com>
2025-07-18 07:53:01 -0300

    Update questão1.c

--- questão1.c
+++ questão1.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
 int main() {
+    int a, b; // a = IVA, b = IRS
+    int liq;
+
+    printf("Digite o valor do IVA (%%): ");
+    scanf("%d", &a);
+
+    printf("Digite o valor do IRS (%%): ");
+    scanf("%d", &b);
+
+    liq = 100 - (a + b);
+
+    printf("\n");
     printf("total = 100%%\n");
-    printf("iva   = 17%%\n");
-    printf("irs   = 15%%\n");
-    printf("liq   = 68%%\n");
+    printf("iva   = %d%%\n", a);
+    printf("irs   = %d%%\n", b);
+    printf("liq   = %d%%\n", liq);
+
     return 0;
 }
-
